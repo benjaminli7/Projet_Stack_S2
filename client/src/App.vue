c83e7be37bbec8e9ec1660efa8aa1e09efa262ec
YALICHEFF Sébastien <yalicheff.sebastien@gmail.com>
2023-06-12 23:28:11 +0200

    Friends Backend

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,7 @@
 <template>
   <div>
     <Navbar v-if="$route.name !== 'Login' && $route.name !== 'Register'" class="w-screen"></Navbar>
+    <Friends v-if="$route.name !== 'Login' && $route.name !== 'Register'" class="w-screen"></Friends>
     <div id="app" class="m-4 p-4">
       <router-view></router-view>
     </div>
