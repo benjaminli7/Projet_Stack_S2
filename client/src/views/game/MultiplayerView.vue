0b5c4d794bd01794eca52c5c7e4c3bb516f2d0d2
benjaminli7 <nicky01.bl@gmail.com>
2023-07-10 08:36:48 +0200

    add rooms to matchmaking

--- client/src/views/game/MultiplayerView.vue
+++ client/src/views/game/MultiplayerView.vue
@@ -1,61 +1,35 @@
 <script setup>
 import { io } from "socket.io-client";
 import { onMounted, ref } from "vue";
-import { Button, Input } from "flowbite-vue"
 import { useUserStore } from "../../userStore";
 
-const messages = ref([]);
-const newMessage = ref("");
 let socket;
 const store = useUserStore();
+const user = store.getUser;
+const loading = ref(true);
 
 onMounted(() => {
-  const user = store.getUser;
-  console.log(user)
   socket = io("http://localhost:3000");
+  socket.emit("playerJoined", user.username)
+  socket.emit("findOpponent")
 
-  socket.on("connection", (message) => {
-    messages.value.push({
-      id: Date.now(),
-      text: message,
-    });
+  socket.on("joinRoom", (roomName) => {
+    console.log("joined room", roomName)
+    socket.emit("joinRoom", roomName)
+  })
+
+  socket.on("gameStarting", () => {
+    loading.value = false;
+    console.log("game starting");
+    // Handle the "gameStarting" event
+    // Perform further actions as needed
   });
 
-  socket.on("disconnection", (message) => {
-    messages.value.push({
-      id: Date.now(),
-      text: message,
-    });
-  });
-
-  socket.on("message", (message) => {
-    messages.value.push(message);
-  });
 });
 
-const sendMessage = () => {
-  if (newMessage.value) {
-    const message = {
-      id: Date.now(),
-      text: newMessage.value,
-    };
-    socket.emit("sendMessage", message);
-    newMessage.value = "";
-  }
-};
 </script>
 
 <template>
-  <h1>Multiplayer view</h1>
-  <Button color="default">Jouer</Button>
-
-  <div>
-    <div v-for="message in messages" :key="message.id">{{ message.text }}</div>
-    <Input
-      type="text"
-      v-model="newMessage"
-      @keyup.enter="sendMessage"
-      placeholder="Type your message"
-    />
-  </div>
+  <p v-if="loading">Searching opponent...</p>
+  <p v-else>Game is starting</p>
 </template>
--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
-
-
+require("dotenv").config();
 
 const db = require("./models");
 
 // Middleware pour parser les requêtes au format JSON
 // allow all CORS requests
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "*");
@@ -21,20 +19,20 @@ app.use(express.urlencoded({ extended: true }));
 db.mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connexion réussie à MongoDB.");
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Erreur de connexion", err);
   });
 
 // Routeur pour les routes d'authentification
-app.use('/auth', require('./routes/auth.routes'));
+app.use("/auth", require("./routes/auth.routes"));
 
 // Routeur pour les routes d'utilisateur
-app.use('/user', require('./routes/user.routes'));
+app.use("/user", require("./routes/user.routes"));
 
 // Routeur /me
 
@@ -43,21 +41,66 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Le serveur écoute sur le port ${process.env.PORT}.`);
 });
 
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
   cors: {
-    origin: '*',
-  }
-})
+    origin: "*",
+  },
+});
+
+let availablePlayers = [];
 
 io.on("connection", function (socket) {
-  io.emit("connection",  `${socket.id} is connected`)
-
-  // Handle disconnections
-  socket.on("disconnect", () => {
-    io.emit("disconnection", `${socket.id} disconnected `);
+  socket.on("playerJoined", (username) => {
+    availablePlayers.push({
+      id: socket.id,
+      username: username,
+    });
   });
 
-  socket.on("sendMessage", (message) => {
-    io.emit("message", message)
-  })
+  socket.on("findOpponent", () => {
+    if (availablePlayers.length >= 2) {
+      const player1 = availablePlayers.shift();
+      const player2 = availablePlayers.shift();
+
+      console.log(player1);
+      console.log(player2);
+
+      // Create a game room
+      const roomName = `${Date.now()}`;
+
+      console.log(roomName);
+
+      // Notify the players to join the room
+      io.to(player1.id).emit("joinRoom", roomName);
+      io.to(player2.id).emit("joinRoom", roomName);
+
+      const socketIds = availablePlayers.filter(player => {
+        return player.username === player1.username || player.username === player2.username
+      })
+      io.to(socketIds).emit("gameStarting");
+
+
+
+      // Remove the players from the available players list
+      availablePlayers = availablePlayers.filter(
+        (player) => player !== player1 && player !== player2
+      );
+    }
+  });
+
+  socket.on("joinRoom", (roomName) => {
+    socket.join(roomName);
+    // You can perform additional actions here, such as starting the game
+
+    // Notify the players that the game is starting
+  });
+
+  // Remove the disconnected player from the players array
+  socket.on("disconnect", () => {
+    players = availablePlayers.filter((player) => player.id !== socket.id);
+  });
+
+  socket.on("joinRoom", (roomName) => {
+    socket.join(roomName);
+  });
 });
