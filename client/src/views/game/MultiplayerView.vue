be161469064a98a36e6d0d81252b05a2695ff2ac
benjaminli7 <nicky01.bl@gmail.com>
2023-07-24 06:34:51 +0200

    add map

--- client/src/views/game/MultiplayerView.vue
+++ client/src/views/game/MultiplayerView.vue
@@ -2,29 +2,35 @@
 import { io } from "socket.io-client";
 import { onMounted, ref } from "vue";
 import { useUserStore } from "../../userStore";
-import MapView from "./MapView.vue";
+import StreetViewMap from "./StreetViewMap.vue";
+import GoogleMap from "./GoogleMap.vue";
 
 let socket;
 const store = useUserStore();
 const user = store.getUser;
 const loading = ref(true);
+let positions = ref([]);
+const round = ref(0);
+let roomName = ref("");
+
 
 onMounted(() => {
   socket = io("http://localhost:3000");
   socket.emit("playerJoined", user.username)
   socket.emit("findOpponent")
 
-  socket.on("joinRoom", (roomName) => {
-    console.log("joined room", roomName)
-    socket.emit("joinRoom", roomName)
+  socket.on("joinRoom", (roomNameData) => {
+    roomName.value = roomNameData;
+    socket.emit("joinRoom", roomNameData)
   })
 
-  socket.on("gameStarting", () => {
+  socket.on("gameStarting", (positionsData) => {
     loading.value = false;
+    positions.value = positionsData;
     console.log("game starting");
-    // Handle the "gameStarting" event
-    // Perform further actions as needed
+
   });
+
 
 });
 
@@ -32,7 +38,15 @@ onMounted(() => {
 
 <template>
   <p v-if="loading">Searching opponent...</p>
-  <p v-else>
-    <MapView />
+  <p id="map-wrapper" v-else>
+    <StreetViewMap :positions="positions" :round="round"/>
+    <GoogleMap :positions="positions" :round="round" :roomName="roomName" />
   </p>
 </template>
+
+<style scoped>
+#map-wrapper {
+  position: relative;
+}
+
+</style>
--- server/index.js
+++ server/index.js
@@ -1,63 +1,3 @@
-// const express = require("express");
-// const app = express();
-// require('dotenv').config();
-// require('module-alias/register')
-
-// const db = require("@models/");
-
-
-
-// // const db = require("./models");
-
-// // Middleware pour parser les requêtes au format JSON
-// // allow all CORS requests
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   next();
-// });
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // Connexion à la base de données MongoDB
-// // db.mongoose
-// //   .connect(process.env.DB_URL, {
-// //     useNewUrlParser: true,
-// //     useUnifiedTopology: true
-// //   })
-// //   .then(() => {
-// //     console.log("Connexion réussie à MongoDB.");
-// //   })
-// //   .catch(err => {
-// //     console.error("Erreur de connexion", err);
-// //   });
-
-// db.sequelize.sync({
-//   alter: true
-// })
-// .then(() => {
-//   console.log('finish alter tables')
-// })
-// .catch(e => {
-//   console.log('dnjncjdkn',e)
-// })
-
-
-// // Routeur pour les routes d'authentification
-// app.use('/auth', require('./routes/auth.routes'));
-
-// // Routeur pour les routes d'utilisateur
-// app.use('/user', require('./routes/user.routes'));
-
-
-// // Routeur /me
-
-// // Démarrage du serveur
-// app.listen(process.env.PORT, () => {
-//   console.log(`Le serveur écoute sur le port ${process.env.PORT}.`);
-// });
-
 const express = require("express");
 const app = express();
 //const GenericRouter = require("./routes/genericCRUD");
@@ -68,27 +8,9 @@ const cors = require("cors");
 var users = require('./routes/user')
 var friends = require('./routes/friend')
 var auth = require('./routes/auth')
+const getRandomPositions = require("./utils");
 
 app.use(cors());
-// app.use(function (req, res, next) {
-//   if (["POST", "PUT", "PATCH"].includes(req.method)) {
-//     if (!req.is("application/json")) {
-//       return res.sendStatus(400);
-//     }
-//   }
-// });
-// const db = require("./models");
-
-// Middleware pour parser les requêtes au format JSON
-// allow all CORS requests
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-// >>>>>>> dev
-//   next();
-// });
-
 app.use(express.json());
 
 app.use("/users", users);
@@ -100,8 +22,6 @@ app.get("/", (req, res) => {
 });
 
 app.use(errorHandler);
-
-// Routeur /me
 
 // Démarrage du serveur
 const server = app.listen(process.env.PORT, () => {
@@ -115,6 +35,7 @@ const io = require("socket.io")(server, {
 });
 
 let availablePlayers = [];
+const rooms = new Map();
 
 io.on("connection", function (socket) {
   socket.on("playerJoined", (username) => {
@@ -129,24 +50,24 @@ io.on("connection", function (socket) {
       const player1 = availablePlayers.shift();
       const player2 = availablePlayers.shift();
 
-      console.log(player1);
-      console.log(player2);
-
       // Create a game room
       const roomName = `${Date.now()}`;
-
-      console.log(roomName);
 
       // Notify the players to join the room
       io.to(player1.id).emit("joinRoom", roomName);
       io.to(player2.id).emit("joinRoom", roomName);
 
-      const socketIds = availablePlayers.filter(player => {
-        return player.username === player1.username || player.username === player2.username
-      })
-      io.to(socketIds).emit("gameStarting");
+      const socketIds = availablePlayers.filter((player) => {
+        return (
+          player.username === player1.username ||
+          player.username === player2.username
+        );
+      });
+
+      const positions = getRandomPositions(5);
 
 
+      io.to(socketIds).emit("gameStarting", positions);
 
       // Remove the players from the available players list
       availablePlayers = availablePlayers.filter(
@@ -157,9 +78,17 @@ io.on("connection", function (socket) {
 
   socket.on("joinRoom", (roomName) => {
     socket.join(roomName);
-    // You can perform additional actions here, such as starting the game
+    if (!rooms.has(roomName)) {
+      rooms.set(roomName, {
+        player1: socket.id,
+        player2: null,
+        player1_guesses: [],
+        player2_guesses: [],
+      });
+    } else {
+      rooms.get(roomName).player2 = socket.id;
+    }
 
-    // Notify the players that the game is starting
   });
 
   // Remove the disconnected player from the players array
@@ -167,20 +96,29 @@ io.on("connection", function (socket) {
     players = availablePlayers.filter((player) => player.id !== socket.id);
   });
 
-  socket.on("joinRoom", (roomName) => {
-    socket.join(roomName);
-  });
+  socket.on("playerGuess", (roomName, guess, round) => {
+    const room = rooms.get(roomName);
+    const currentPlayer = room.player1 === socket.id ? "player1" : "player2";
+
+    if(currentPlayer === "player1"){
+      room.player1_guesses.push(guess);
+    } else {
+      room.player2_guesses.push(guess);
+    }
+
+
+    // if one of the two players has guessed, notify the second player
+    if(room.player1_guesses.length === round + 1 || room.player2_guesses.length === round + 1){
+      socket.to(roomName).emit("opponentGuessed", guess);
+    }
+
+
+
+    if(room.player1_guesses.length === 5 && room.player2_guesses.length === 5){
+      socket.to(roomName).emit("gameFinished", room);
+    }
+
+
+  })
+
 });
-
-
-app.use('/users', users)
-app.use('/friends', friends)
-app.use('/auth', auth)
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-
-app.use(errorHandler);
-
