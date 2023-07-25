9859322225db318cc92ce0a23c20250d6026e73a
agraval <alexandre.samson78@gmail.com>
2023-07-25 15:44:37 +0200

    Payment lv up

--- client/src/views/user/Premium.vue
+++ client/src/views/user/Premium.vue
@@ -1,73 +1,74 @@
 <template>
-  <div>
-    <h2>Devenez Premium</h2>
-    <button @click="handleCheckout('premiumPackage', 'Premium Package', 10)" :disabled="isPurchased">Acheter Premium pour 10$</button>
-    <p v-if="isPurchased">Vous avez déjà acheté ce package Premium.</p>
-    <p v-if="this.$route.query.status === 'cancel'">Le paiement a été annulé.</p>
+  <div class="container mx-auto p-4">
+    <h2 class="text-xl font-semibold mb-4">Devenez Premium</h2>
+    <div class="flex justify-center">
+      <div class="border p-4 rounded shadow-md">
+        <h3 class="text-lg font-medium mb-2">Premium Package</h3>
+        <p class="mb-4">Obtenez des avantages Premium pour seulement 10$.</p>
+        <button
+            @click="handleCheckout('premiumPackage', 'Premium Package', 10)"
+            :disabled="isPurchased"
+            class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed">
+          Acheter Premium pour 10$
+        </button>
+      </div>
+    </div>
+    <p v-if="isPurchased" class="mt-4">Vous avez déjà acheté ce package Premium.</p>
+    <p v-if="$route.query.status === 'cancel'" class="mt-4 text-red-500">Le paiement a été annulé.</p>
   </div>
 </template>
 
-<script>
+<script setup>
 import axios from "axios";
+import { ref, onMounted } from 'vue';
 
-export default {
-  name: 'Premium',
-  data() {
-    const user = JSON.parse(localStorage.getItem('user'));
-    return {
-      stripe: {},
-      userId: user ? user.id : null,
-      isPurchased: false,
-    };
-  },
+const user = JSON.parse(localStorage.getItem('user'));
+const stripe = ref({});
+const userId = ref(user ? user.id : null);
+const isPurchased = ref(false);
 
-  mounted() {
-    this.stripe = window.Stripe('pk_test_51NWhaQBS812DNqMjMpIqeLQEP5wSNeht3MpufatBZCAX4aLD0RfSnteFshIEoBXzmhTTasJ9JVK2mERRkhRE0K0r00D1p0P7lP');
-  },
-
-  async created() {
-    if (this.userId) {
-      try {
-        this.isPurchased = await this.checkIfItemPurchased(this.userId);
-      } catch (error) {
-        console.error("Une erreur est survenue lors de la vérification des achats.", error);
+const handleCheckout = async (itemId, itemName, amount) => {
+  try {
+    const { data } = await axios.post("http://127.0.0.1:3000/stripe/checkout", {
+      userId: userId.value.toString(),
+      itemId: itemId,
+      itemName: itemName,
+      amount: amount
+    });
+    stripe.value.redirectToCheckout({
+      sessionId: data.sessionId
+    }).then(result => {
+      if (result.error) {
+        console.error(result.error.message);
       }
-    }
-  },
-
-  methods: {
-    async handleCheckout(itemId, itemName, amount) {
-      try {
-        const { data } = await axios.post("http://127.0.0.1:3000/stripe/checkout", {
-          userId: this.userId.toString(),
-          itemId: itemId,
-          itemName: itemName,
-          amount: amount
-        });
-        this.stripe.redirectToCheckout({
-          sessionId: data.sessionId
-        }).then(function (result) {
-          if (result.error) {
-            console.error(result.error.message);
-          }
-        });
-      } catch (error) {
-        console.error("Une erreur est survenue lors de la création de l'intention de paiement.", error);
-      }
-    },
-
-    async checkIfItemPurchased(userId) {
-      try {
-        const { data } = await axios.get(`http://127.0.0.1:3000/stripe/check-purchase/${userId}`);
-        return data.purchased;
-      } catch (error) {
-        console.error("Une erreur est survenue lors de la vérification des achats.", error);
-        return false;
-      }
-    },
+    });
+  } catch (error) {
+    console.error("Une erreur est survenue lors de la création de l'intention de paiement.", error);
   }
 };
+
+const checkIfItemPurchased = async (userId) => {
+  try {
+    const { data } = await axios.get(`http://127.0.0.1:3000/stripe/check-purchase/${userId}`);
+    return data.purchased;
+  } catch (error) {
+    console.error("Une erreur est survenue lors de la vérification des achats.", error);
+    return false;
+  }
+};
+
+onMounted(async () => {
+  stripe.value = window.Stripe('pk_test_51NWhaQBS812DNqMjMpIqeLQEP5wSNeht3MpufatBZCAX4aLD0RfSnteFshIEoBXzmhTTasJ9JVK2mERRkhRE0K0r00D1p0P7lP');
+  if (userId.value) {
+    try {
+      isPurchased.value = await checkIfItemPurchased(userId.value);
+    } catch (error) {
+      console.error("Une erreur est survenue lors de la vérification des achats.", error);
+    }
+  }
+});
 </script>
 
 <style scoped>
+/* Vous pouvez ajouter des styles supplémentaires si nécessaire */
 </style>
