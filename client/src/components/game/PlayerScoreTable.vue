170031e8da3ea407d42ce82c7d877fdfc5123126
benjaminli77 <nicky01.bl@gmail.com>
2023-07-26 11:26:08 +0200

    change text color

--- client/src/components/game/PlayerScoreTable.vue
+++ client/src/components/game/PlayerScoreTable.vue
@@ -33,7 +33,7 @@ const props = defineProps({
           <tr v-for="(entry, index) in props.guesses" :key="index">
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              class="px-6 py-4 font-medium text-gray-700 whitespace-nowrap"
             >
               {{ index + 1 }}
             </th>
@@ -42,7 +42,7 @@ const props = defineProps({
           <tr>
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              class="px-6 py-4 font-medium text-gray-700 whitespace-nowrap"
             >
               Total
             </th>
