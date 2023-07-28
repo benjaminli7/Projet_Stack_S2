a6e830e5e70f06ba224e00e95167cbe3d2161159
benjaminli7 <nicky01.bl@gmail.com>
2023-07-28 04:18:39 +0200

    test

--- server/index.js
+++ server/index.js
@@ -37,20 +37,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "https://challengeguessr.ovh/",
-    ],
-  },
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-      "Access-Control-Allow-Credentials": true,
-    };
-    res.writeHead(200, headers);
-    res.end();
+    origin: "*",
   },
 });
 
